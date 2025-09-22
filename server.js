bb2ffc26aa74a01833bb4515a234ea3e52a6c4bf
Developer-cocopalms <developer@cocopalms.io>
2025-09-22 12:15:36 +0300

    ADDED Fallback port

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ require('dotenv').config()
 ConnectDb()
 const app = express()
 
-const port = process.env.PORT
+const port = process.env.PORT || 4000
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
