8a4328e6c637f01cfd208ee34fbbbe04dbc7d690
Developer-cocopalms <developer@cocopalms.io>
2025-09-23 09:38:21 +0300

    allowed only specific forntend becuse of cookies issue

--- server.js
+++ server.js
@@ -9,14 +9,22 @@ const app = express()
 
 const port = process.env.PORT || 4000
 
-// const allowedOrigins = [
-//     'http://localhost:5175', // Example: your frontend development server
-//     'https://your-production-frontend.com' // Example: your live site
-// ];
+const allowedOrigins = [
+    'http://localhost:5175', // Example: your frontend development server
+    'http://localhost:5173', // Example: your frontend development server
+    'http://localhost:3000', // Example: your frontend development server
+    'https://your-production-frontend.com' // Example: your live site
+];
 
 const corsOptions = {
-    origin: "*",
-    credentials: true, // This is important if you're using cookies or sessions
+    origin: function (origin, callback) {
+        if (!origin) return callback(null, true); // Allow REST tools or same origin requests
+        if (allowedOrigins.indexOf(origin) !== -1) {
+            callback(null, origin); // Reflect the requested origin
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },    credentials: true, // This is important if you're using cookies or sessions
 };
 
 // Use the cors middleware with the configured options
